43d7bba56a8de206274af940353fa294f84350ec
User <xtoycohcohu@mail.ru>
2024-04-30 18:16:59 +0700

    калькулятор/gth

--- src/main/java/pro/sky/calculator3/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator3/controller/CalculatorController.java
@@ -1,9 +1,21 @@
 package pro.sky.calculator3.controller;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.calculator3.service.CalculatorService;
 
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService){
+        this.calculatorService = calculatorService;
+    }
+
     @GetMapping(value = "/calculator", produces = MediaType.TEXT_HTML_VALUE)
     public String calculator() {
         return "<h1>Добро пожаловать в калькулятор!</h1>";
@@ -17,7 +29,7 @@ public class CalculatorController {
         } else if (num2 == null) {
             return "<p style=\"color: red\">Параметр num2 не передан!</p>";
         } else {
-            return num1 + " + " + num2 + " = " + (num1+num2);
+            return num1 + " + " + num2 + " = " + calculatorService.plus(num1, num2);
         }
     }
     @GetMapping(value = "/minus", produces = MediaType.TEXT_HTML_VALUE)
@@ -28,7 +40,7 @@ public class CalculatorController {
         } else if (num2 == null) {
             return "<p style=\"color: red\">Параметр num2 не передан!</p>";
         } else {
-            return num1 + " - " + num2 + " = " + (num1-num2);
+            return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
         }
     }
 
@@ -40,7 +52,7 @@ public class CalculatorController {
         } else if (num2 == null) {
             return "<p style=\"color: red\">Параметр num2 не передан!</p>";
         } else {
-            return num1 + " * " + num2 + " = " + (num1*num2);
+            return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
         }
     }
 
@@ -54,7 +66,7 @@ public class CalculatorController {
         } else if (num2 == 0) {
             return "<p style=\"color: red\">На 0 делить нельзя!</p>";
         } else {
-            return num1 + " / " + num2 + " = " + (num1/num2);
+            return num1 + " / " + num2 + " = " + calculatorService.divide(num1, num2);
         }
     }
 }
